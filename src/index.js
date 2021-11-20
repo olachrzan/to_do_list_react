28b3f53eabcc342f91cc79e2ec45e5ffdea8e8b1
Ola Chrzan <ola.chrzan16@gmail.com>
2021-11-20 19:44:50 +0100

    Add store.js and Provider to index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 import { theme } from './theme'
 import { GlobalStyle } from './GlobalStyle';
+import store from './store';
 
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <App />
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <App />
+      </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

d00f247b140e73cb37bf2d4d828c42ccfe9d2133
Ola Chrzan <ola.chrzan16@gmail.com>
2021-11-21 18:37:10 +0100

    Add other reducers to slice

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -2,14 +2,33 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const tasksSlice = createSlice({
   name: "tasks",
-  initialState: { tasks: [] },
+  initialState: {
+    tasks: [],
+    hideDone: false,
+  },
   reducers: {
     addTask: ({ tasks }, { payload }) => {
       tasks.push(payload);
     },
+    removeTask: ({ tasks }, { payload }) => {
+      const index = tasks.findIndex(({ id }) => id === payload);
+      tasks.splice(index, 1)
+    },
+    toggleTaskDone: ({ tasks }, { payload }) => {
+      const index = tasks.findIndex(({ id }) => id === payload);
+      tasks[index].done = !tasks[index].done
+    },
+    toggleHideDone: state => {
+      state.hideDone = !state.hideDone;
+    },
+    setAllDone: ({ tasks }) => {
+      for (const task of tasks) {
+        task.done = true;
+      }
+    }
   }
 });
 
-export const { addTask } = tasksSlice.actions;
+export const { addTask, removeTask, toggleTaskDone, toggleHideDone, setAllDone } = tasksSlice.actions;
 export const selectTasks = state => state.tasks;
 export default tasksSlice.reducer;

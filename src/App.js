dda0dee4301e9d3324760407ad5b9d2d70d82f9e
Ola Chrzan <ola.chrzan16@gmail.com>
2021-12-08 11:45:24 +0100

    Change BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom"
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom"
 import Navigation from "./common/Navigation"
 import AuthorPage from "./features/author"
 import TasksPage from "./features/tasks/TasksPage";
 import TaskPage from "./features/tasks/TaskPage";
 
 export const App = () => (
-  <BrowserRouter basename="/to_do_list_react">
+  <HashRouter>
     <Navigation />
     <Switch>
       <Route path="/zadania/:id">
@@ -21,5 +21,5 @@ export const App = () => (
         <Redirect to="/zadania" />
       </Route>
     </Switch>
-  </BrowserRouter>
+  </HashRouter>
 )
